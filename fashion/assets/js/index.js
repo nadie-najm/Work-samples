db1ea44cdcde2a1fd66f3b0315b50f3bba936d9e
meganoob <kasrahabibbeygi@gmail.com>
2020-07-25 09:31:40 -0700

    asdasd

--- fashion/assets/js/index.js
+++ fashion/assets/js/index.js
@@ -1,26 +1,30 @@
 $('.bottom_of_menu_bar').click(function () {
 
     new WOW().init();
-    setInterval(function () {
+
+    $('#first_text_about_uniti').fadeOut();
+    $('.bottom_of_menu_bar').fadeOut();
+    $('.bottom_of_close').fadeIn(700);
+
+    setTimeout(function () {
 
         $('.menu_options').show();
 
-    }, 1000);
-
-    $('#first_text_about_uniti').fadeOut();
-    $('.bottom_of_menu_bar').fadeOut(2000);
-    $('.bottom_of_close').fadeIn(2000);
-
+    }, 500);
 
 });
 
 $('.bottom_of_close').click(function () {
 
-
-    $('#first_text_about_uniti').fadeIn(2000);
     $('.menu_options').fadeOut();
-    $('.bottom_of_close').fadeOut(2000);
-    $('.bottom_of_menu_bar').fadeIn(2000);
 
+    setTimeout(function () {
+
+        $('#first_text_about_uniti').fadeIn();
+
+    }, 500);
+
+    $('.bottom_of_close').fadeOut();
+    $('.bottom_of_menu_bar').fadeIn();
 
 });
