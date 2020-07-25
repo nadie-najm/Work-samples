4e12f2f47fecd2e69f118e66d7bed6762581325e
nadie-najm <nadi.codes@gmail.com>
2020-07-25 19:09:24 +0430

    header of home page edit responsive

--- fashion/assets/js/index.js
+++ fashion/assets/js/index.js
@@ -3,7 +3,6 @@ $('.bottom_of_menu_bar').click(function () {
     new WOW().init();
     setInterval(function () {
 
-        $('.ul_menu_box').show();
         $('.menu_options').show();
 
     }, 1000);
@@ -19,7 +18,6 @@ $('.bottom_of_close').click(function () {
 
 
     $('#first_text_about_uniti').fadeIn(2000);
-    $('.ul_menu_box').fadeOut();
     $('.menu_options').fadeOut();
     $('.bottom_of_close').fadeOut(2000);
     $('.bottom_of_menu_bar').fadeIn(2000);
