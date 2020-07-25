b7e926f40bf54770a8e8f5a261c4547bbf14fa66
nadie-najm <nadi.codes@gmail.com>
2020-07-25 17:29:09 +0430

    index ja

--- fashion/assets/js/index.js
+++ fashion/assets/js/index.js
@@ -1,14 +1,28 @@
-function click_on_menu_bar()
-{
+$('.bottom_of_menu_bar').click(function () {
+
     new WOW().init();
-    setInterval(function ()
-    {
-        document.getElementById('first_text_about_uniti').style.display='none';
-        let menu_options= document.getElementsByClassName('menu_options');
-        for (let i=0 ; i<menu_options.length ; i++)
-        {
-            menu_options[i].style.display='block';
-        }
-    }
-    ,1000)
-}
+    setInterval(function () {
+
+        $('.ul_menu_box').show();
+        $('.menu_options').show();
+
+    }, 1000);
+
+    $('#first_text_about_uniti').fadeOut();
+    $('.bottom_of_menu_bar').fadeOut(2000);
+    $('.bottom_of_close').fadeIn(2000);
+
+
+});
+
+$('.bottom_of_close').click(function () {
+
+
+    $('#first_text_about_uniti').fadeIn(2000);
+    $('.ul_menu_box').fadeOut();
+    $('.menu_options').fadeOut();
+    $('.bottom_of_close').fadeOut(2000);
+    $('.bottom_of_menu_bar').fadeIn(2000);
+
+
+});
